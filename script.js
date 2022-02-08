3797e98fb2e001636f2dc287a42f4a3332c6d570
Richard Ye <33409792+richardye101@users.noreply.github.com>
2022-02-08 15:52:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function compute()
 {
     var principal = document.getElementById("principal").value;
-    if(document.getElementById("principal") < 1){
+    if(principal < 1){
         alert("Enter a positive number");
         document.getElementById("principal").focus();
     }
@@ -9,11 +9,11 @@ function compute()
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>";
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
 }
 
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+    document.getElementById("rate_val").innerText=rateval+"%";
 }
