93a05955ac8b2d44eebd226d4659c289608db4af
Richard Ye <33409792+richardye101@users.noreply.github.com>
2022-02-08 15:29:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -22,10 +22,6 @@ function validatePrincipal()
         alert("Enter a positive number");
         document.getElementById("principal").focus();
     }
-    else
-    {
-        updateInterest();
-    }
 }
 
 function compute()
