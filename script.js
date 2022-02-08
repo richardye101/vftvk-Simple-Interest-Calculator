8ecb31445313156fe35f7aaa8d0ea3eda620753a
Richard Ye <33409792+richardye101@users.noreply.github.com>
2022-02-08 15:45:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,19 @@
-var principal = document.getElementById("principal").value;
-var rate = document.getElementById("rate").value;
-var years = document.getElementById("years").value;
-var interest = principal * years * rate /100;
-var year = new Date().getFullYear()+parseInt(years);
+function compute()
+{
+    var principal = document.getElementById("principal").value;
+    if(document.getElementById("principal") < 1){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>";
+}
 
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval + "%";
-}
-
-function validatePrincipal()
-{
-    if(document.getElemetnById("principal") < 1)
-    {
-        alert("Enter a positive number");
-        document.getElementById("principal").focus();
-    }
-}
-
-function compute()
-{
-    principal = document.getElementById("principal").value;
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>";
+    document.getElementById("rate_val").innerText=rateval;
 }
